95018a1eccafb6465a042d18ae68a4fda293bbba
whtjqls0504 <whtjqls0504@naver.com>
2023-10-13 10:09:20 +0900

    231013

--- 13_file/src/main/java/com/gdu/app13/util/MyFileUtil.java
+++ 13_file/src/main/java/com/gdu/app13/util/MyFileUtil.java
@@ -13,11 +13,15 @@ public class MyFileUtil {
     /*    /storage/yyyy/MM/dd    */
     LocalDate today = LocalDate.now();
     return "/storage/" + today.getYear() + "/" + String.format("%02d", today.getMonthValue()) + "/" + String.format("%02d", today.getDayOfMonth());
+    // return "/storage/" + DateTimeFormatter.ofPattern("yyyy/MM/dd").format(today);
+  
   }
   
   // 파일에 저장될 이름 반환하기
   public String getFilesystemName(String originalName) {
+    
     /*  UUID.확장자  */
+    
     String extName = null;
     if(originalName.endsWith("tar.gz")) {     // 확장자에 마침표가 포함되는 예외 경우를 처리한다.
       extName = "tar.gz";                     // 확장자
