1c2d0934699d3de30731d7e27b7e8f1d910e1f66
whtjqls0504 <whtjqls0504@naver.com>
2023-10-13 10:00:25 +0900

    231013

---
+++ 13_file/src/main/java/com/gdu/app13/util/MyFileUtil.java
@@ -0,0 +1,37 @@
+package com.gdu.app13.util;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyFileUtil {
+
+  // 파일이 저장될 경로 반환하기
+  public String getPath() {
+    /*    /storage/yyyy/MM/dd    */
+    LocalDate today = LocalDate.now();
+    return "/storage/" + today.getYear() + "/" + String.format("%02d", today.getMonthValue()) + "/" + String.format("%02d", today.getDayOfMonth());
+  }
+  
+  // 파일에 저장될 이름 반환하기
+  public String getFilesystemName(String originalName) {
+    /*  UUID.확장자  */
+    String extName = null;
+    if(originalName.endsWith("tar.gz")) {     // 확장자에 마침표가 포함되는 예외 경우를 처리한다.
+      extName = "tar.gz";                     // 확장자
+    } else {
+      String[] arr = originalName.split(".");         // [.] 또는 \\.
+      extName = arr[arr.length - 1];
+      // regex는? 정규식 입력하란 뜻. (regular Expression?) 정규식에서 마침표는 모든 문자라고 하는 메타문자 성질을 갖고있다.
+    }
+    
+    return UUID.randomUUID().toString().replace("-", "") + "." + extName;
+    
+    
+    
+    
+  }
+  
+}
