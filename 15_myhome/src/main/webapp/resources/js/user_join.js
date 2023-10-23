390f9e9b6095c4b3d09233c7921d6ae535b27bee
whtjqls0504 <whtjqls0504@naver.com>
2023-10-23 10:03:11 +0900

    231023

--- 15_myhome/src/main/webapp/resources/js/user_join.js
+++ 15_myhome/src/main/webapp/resources/js/user_join.js
@@ -5,12 +5,14 @@
 /* 함수 호출 */
 $(() => {
   fnCheckEmail();
+  fnCheckPassword();
   fnJoin();
 })
 
 
 /* 전역변수 선언 */
 var emailPassed = false;
+var pwPassed = false;
 
 
 /* 함수 정의 */
@@ -93,10 +95,31 @@ const fnCheckEmail = () => {
   
 }
 
+const fnCheckPassword = () => {
+  // 비밀번호 : 8~20자, 영문,숫자,특수문자, 2가지 이상 포함
+  let validPwCount = /[A-Z]/.test(pw)          // 대문자가 있으면   true
+                   + /[a-z]/.test(pw)          // 소문자가 있으면   true
+                   + /[0-9]/.test(pw)          // 숫자가 있으면     true
+                   + /[^A-Za-z0-9]/.test(pw);  // 특수문자가 있으면 true
+  $('#pw').keyup((ev) => {
+    let pw = $(ev.target).val();
+    pwPassed = pw.length >= 8 && pw.length <= 20 && validPwCount >= 2;
+    if(pwPassed){
+      $('#msg_pw').text('사용 가능한 비밀번호입니다.');
+    } else {
+      $('#msg_pw').text('비밀번호는 8~20자, 영문/숫자/특수문자를 2가지 이상 포함해야 합니다.');       
+    }
+  })
+}
+
 const fnJoin = () => {
   $('#frm_join').submit((ev) => {
     if(!emailPassed){
       alert('이메일을 인증 받아야 합니다.');
+      ev.preventDefault();
+      return;
+    } else if(!pwPassed){
+      alert('비밀번호를 확인하세요.');
       ev.preventDefault();
       return;
     }
